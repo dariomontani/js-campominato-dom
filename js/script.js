a3da5409913c64a080771411310f20a858d1aeed
dariomontani <dariomontani@gmail.com>
2021-12-02 16:21:38 +0100

    16 numeri random riga 74

--- js/script.js
+++ js/script.js
@@ -64,3 +64,28 @@ play.addEventListener('click', function(){
         }
     }
 });
+
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+
+// genero 16 numeri casuali senza duplicati
+function numeriRandom(min, max) {
+    const arraySedici = [];
+    let numeriCasuali = Math.floor(Math.random() * (max - min)) + min;
+    for (let i = 0; i < 16; i++) {
+        while (!(arraySedici.includes(numeriCasuali))) {
+            arraySedici.push(numeriCasuali);
+            i++;
+        }
+    }
+    return arraySedici;
+}
+
+let sediciNumeri = numeriRandom(1, 16);
+console.log(arraySedici);
+
+// quadrati.addEventListener('click', function(){
+
+// });
